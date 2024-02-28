40c486e19de0f8185038cf57585a90c12022b577
Seko461 <sekopierre@gmail.com>
2024-02-28 16:53:27 +0100

    trying to change selection

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,12 +1,16 @@
-let grid = document.getElementById('grid');
+let grid = document.getElementById('grid_1');
 let totalCells = 100;
 let numbers = [];
 let startBtn = document.getElementById('start');
+let easy = document.getElementById('easy');
+let medium = document.getElementById('medium');
 
 
 
 
-startBtn.addEventListener('click', function () {
+
+
+function easyLevel() {
     for (let i = 1; i <= totalCells; i++) {
         //creao le celle
 
@@ -25,8 +29,31 @@ startBtn.addEventListener('click', function () {
             console.log(cell);
 
         });
+
     };
+}
 
+function mediumLevel() {
+    let totalCells = 81;
+    let grid = document.getElementById('grid_2');
+    for (let i = 1; i <= totalCells; i++) {
+        //creao le celle
 
-})
+        let cell = document.createElement('div');
+        let markup = `${i}`;
+        cell.innerHTML += markup;
+        cell.classList.add('cell');
+        // console.log(cell);
+        grid.appendChild(cell);
 
+        //Gestiamo il click della cella e diamo colore
+        cell.addEventListener('click', function () {
+            console.log('click');
+
+            cell.classList.add('cell-clicked');
+            console.log(cell);
+
+        });
+
+    };
+}
