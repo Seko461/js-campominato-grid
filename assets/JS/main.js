b17477371318d60abdfa0c79062dc91fa05699be
Seko461 <sekopierre@gmail.com>
2024-02-28 16:05:35 +0100

    cell clicked class style & click function on cell

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,15 +1,32 @@
 let grid = document.getElementById('grid');
 let totalCells = 100;
 let numbers = [];
+let startBtn = document.getElementById('start');
 
-for (let i = 1; i <= totalCells; i++) {
-    //creao le celle
 
-    let cell = document.createElement('div');
-    let markup = `<div class="cell">${i}</div>`;
-    cell.innerHTML += markup;
-    cell.classList.add('cell');
-    console.log(cell);
-    grid.appendChild(cell);
-};
+
+
+startBtn.addEventListener('click', function () {
+    for (let i = 1; i <= totalCells; i++) {
+        //creao le celle
+
+        let cell = document.createElement('div');
+        let markup = `${i}`;
+        cell.innerHTML += markup;
+        cell.classList.add('cell');
+        // console.log(cell);
+        grid.appendChild(cell);
+
+        //Gestiamo il click della cella e diamo colore
+        cell.addEventListener('click', function () {
+            console.log('click');
+
+            cell.classList.add('cell-clicked');
+            console.log(cell);
+
+        });
+    };
+
+
+})
 
