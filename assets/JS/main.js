1d89e1f9db57f5d244efe6364889694469f666df
Seko461 <sekopierre@gmail.com>
2024-02-28 15:36:09 +0100

    add dynamic cells

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,2 +1,13 @@
-let grid = document.querySelector('.grid');
+let grid = document.getElementById('grid');
+let totalCells = 100;
+let numbers = [];
 
+for (let i = 1; i <= totalCells; i++) {
+    //creao le celle
+
+    let cell = document.createElement('div');
+
+    cell.classList.add('cell');
+    console.log(cell);
+    grid.appendChild(cell);
+}
