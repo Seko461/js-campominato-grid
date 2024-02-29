1de22a3fe92d5160a7e40357c6a6857a080fdd1c
Seko461 <sekopierre@gmail.com>
2024-02-29 06:19:33 +0100

    fixed

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,119 +1,52 @@
+const main = document.getElementById('grid_container');
+const startBtn = document.getElementById('start');
+const levelSelected = document.getElementById('levels');
+
+let levels = [100, 81, 49]
+let totalCells;
 
 
-// let numbers = [];
-let startBtn = document.getElementById('start');
-
-let levels = document.getElementById('levels');
-let easy = document.getElementById('easy');
-let medium = document.getElementById('medium');
-let hard = document.getElementById('hard');
+startBtn.addEventListener('click', play);
 
 
-// console.log(easy, medium);
+// ----- Functions ----- //
 
+function play() {
+    totalCells = levels[levelSelected.value]
+    reset();
+    gridGenerator();
+}
 
+function gridGenerator() {
+    const grid = document.createElement('div');
+    grid.classList.add('grid')
 
-
-levels.addEventListener('change', function () {
-
-    console.log(this.value);
-
-    if (levels.value === easy.value) {
-        console.log('questo è easy');
-        startBtn.addEventListener('click', easyLevel());
-
-
-    } else if (levels.value === medium.value) {
-        console.log('questo è medium');
-        startBtn.addEventListener('click', mediumLevel());
+    for (let i = 0; i < totalCells; i++) {
+        const cell = createCell(i);
+        grid.append(cell)
     }
-    if (levels.value === hard.value) {
-        console.log('questo è hard');
-        startBtn.addEventListener('click', hardLevel());
 
+    main.append(grid)
+}
+
+function createCell(i) {
+    const cell = document.createElement('div');
+    cell.className = 'cell';
+    cell.classList.add('cell' + totalCells);
+    let markup = `${i + 1}`
+    cell.innerHTML += markup
+
+    cell.addEventListener('click', handlerClickCell)
+
+    function handlerClickCell() {
+        this.classList.toggle('clicked');
+
+        console.log(markup);
     }
-});
 
+    return cell;
+}
 
-
-function easyLevel() {
-    let totalCells = 100;
-    let grid = document.getElementById('grid_1');
-    startBtn.addEventListener('click', function () {
-
-        for (let i = 1; i <= totalCells; i++) {
-            //creao le celle
-
-            let cell = document.createElement('div');
-            let markup = `${i}`;
-            cell.innerHTML += markup;
-            cell.classList.add('cell');
-            // console.log(cell);
-            grid.appendChild(cell);
-
-            //Gestiamo il click della cella e diamo colore
-            cell.addEventListener('click', function () {
-                console.log('click');
-
-                cell.classList.add('cell-clicked');
-                console.log(cell);
-
-            });
-
-        };
-    })
-};
-
-function mediumLevel() {
-    let totalCells = 81;
-    let grid = document.getElementById('grid_2');
-    startBtn.addEventListener('click', function () {
-        for (let i = 1; i <= totalCells; i++) {
-            //creao le celle
-
-            let cell = document.createElement('div');
-            let markup = `${i}`;
-            cell.innerHTML += markup;
-            cell.classList.add('cell');
-            // console.log(cell);
-            grid.appendChild(cell);
-
-            //Gestiamo il click della cella e diamo colore
-            cell.addEventListener('click', function () {
-                console.log('click');
-
-                cell.classList.add('cell-clicked');
-                console.log(cell);
-
-            });
-
-        };
-    })
-};
-
-function hardLevel() {
-    let totalCells = 49;
-    let grid = document.getElementById('grid_3');
-    startBtn.addEventListener('click', function () {
-        for (let i = 1; i <= totalCells; i++) {
-            //creao le celle
-
-            let cell = document.createElement('div');
-            let markup = `${i}`;
-            cell.innerHTML += markup;
-            cell.classList.add('cell');
-            // console.log(cell);
-            grid.appendChild(cell);
-
-            //Gestiamo il click della cella e diamo colore
-            cell.addEventListener('click', function () {
-                console.log('click');
-
-                cell.classList.add('cell-clicked');
-                console.log(cell);
-
-            });
-
-        };
-    })
-};
+function reset() {
+    main.innerHTML = '';
+}
